95ffc8a824bd93fabf3ddec0844d2187706a0770
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-15 18:28:53 +0700

    Implement search pokemon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
@@ -9,5 +9,90 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
+let pokedex = {
+  nextId: 10,
+  data: [
+    {
+      id: 1,
+      name: "Snivy",
+      type: "Grass"
+    },
+    {
+      id: 2,
+      name: "Tepig",
+      type: "Fire"
+    },
+    {
+      id: 3,
+      name: "Oshawott",
+      type: "Water"
+    },
+    {
+      id: 4,
+      name: "Patrat",
+      type: "Normal"
+    },
+    {
+      id: 5,
+      name: "Herdier",
+      type: "Normal"
+    },
+    {
+      id: 6,
+      name: "Liepard",
+      type: "Dark"
+    },
+    {
+      id: 7,
+      name: "Pansear",
+      type: "Fire"
+    },
+    {
+      id: 8,
+      name: "Palpitoad",
+      type: "Water"
+    },
+    {
+      id: 9,
+      name: "Sandile",
+      type: "Dark"
+    }
+  ]
+};
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "Hello World",
+    headers: req.headers
+  });
+});
+
+//Get All Pokemon Name
+
+app.get("/pokedex", (req, res) => {
+  res.send({
+    count: pokedex.data.length,
+    data: pokedex.data
+  });
+});
+
+//Search Pokemon by Name and Type
+
+app.get("/pokedex/search", (req, res) => {
+  const queryName = req.query.name || ""; // if queryName haven't value , default value is "" 
+  const queryType = req.query.type || ""; // if .includes have "" value , default value will be "true"
+
+  const findPokedex = pokedex.data.filter(pokemon => {
+    return (
+      pokemon.name.toLowerCase().includes(queryName.toLowerCase()) &&
+      pokemon.type.toLowerCase().includes(queryType.toLowerCase())
+      // every name at data have "" value
+    );
+  });
+
+  res.send({
+    data: findPokedex
+  });
+});
 
 app.listen(port, () => console.log("Server running at localhost:3000"));
