4d2404a8fe5c68fbfc5a4e23f849aa4759dfe87e
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-15 16:57:21 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors")
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+
+app.listen(port, () => console.log("Server running at localhost:3000"));
